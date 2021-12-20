734dd9d21e54c9b459ae36e08aeca62d1cdf39f9
Wisp <58921058+SKOWisp@users.noreply.github.com>
2021-12-20 01:07:52 -0600

    Update to regex
    Regex now matches with more cases in exchange for performace.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,8 +6,8 @@ module.exports = {
     execute(message) {
         if (message.author.bot) return;
 
-        const regNYA = new RegExp(/\bn+(\W|_){0,8}[y]+([y]|\1){0,16}[a4]+([a4]|\1){0,16}?[nrsz]*\b/, 'gi');
-        const regOWVU= new RegExp(/\b[tou7]+(\W|_){0,8}[vw]+([vw]|\1){0,16}[tou7]+\1{0,8}?[nrsz]*\b/, 'gi');
+        const regNYA = /\bn+[y\p{P}]+?a+[nrsz]*?\b/giu
+        const regOWVU= /\b[tou07]+[vw\p{P}]+?[tou07]+[nrsz]*\b/giu
 
         //No lo cuestiones, sólo gózalo (Quien sabe como jala, pero jala)
         const aberracionEnStr = (str) => {
